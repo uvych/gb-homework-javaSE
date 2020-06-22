4187287dd533b0c9233566685d396d8cdb12edb8
Georgy <georgy143w@gmail.com>
2020-06-22 18:06:34 +0300

    Add lesson3 homework

---
+++ src/main/java/com/geekbrains/georgy/lesson3/Move.java
@@ -0,0 +1,6 @@
+package com.geekbrains.georgy.lesson3;
+
+public interface Move {
+   boolean jump(int height);
+   boolean run(int distance);
+}
---
+++ src/main/java/com/geekbrains/georgy/lesson3/Obstacle.java
@@ -0,0 +1,5 @@
+package com.geekbrains.georgy.lesson3;
+
+public interface Obstacle {
+    boolean tryToOvercome(Move mover);
+}
