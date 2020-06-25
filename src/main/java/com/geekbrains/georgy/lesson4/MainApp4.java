4be68a451a8068453bbd99f1732b6b969695c244
Georgy <georgy143w@gmail.com>
2020-06-25 14:27:09 +0300

    Add homework lesson4

---
+++ src/main/java/com/geekbrains/georgy/lesson4/MainApp4.java
@@ -0,0 +1,36 @@
+package com.geekbrains.georgy.lesson4;
+
+public class MainApp4 {
+    public static void main(String[] args) {
+        /* String [][] strings = {{"3","4" ,"2","0"},{"4","2" , "2" ,"2"},{"4","2" , "2" ,"2"}};*/  /*MyArraySizeException*/
+        /*String [][] strings = {{"3","4" ,"2","0"},{"4","2" , "2" ,"2"},{"4","2" , "2" ,"2"},{"4","2" , "2" ,"2"}};*/  /*39*/
+        String [][] strings = {{"3","f" ,"2","0"},{"4","2" , "2" ,"2"},{"4","2" , "2" ,"2"},{"4","2" , "2" ,"2"}};  /*MyArrayDataException*/
+
+        try {
+            sumArrayElement(strings);
+        }catch (MyArraySizeException | MyArrayDataException e ){
+            e.printStackTrace();
+        }
+    }
+
+    public static void sumArrayElement(String [][] array){
+        checkArray(array);
+        int sum = 0;
+
+        for (String [] string : array) {
+            for (String strNumber : string) {
+                try {
+                    sum += Integer.parseInt(strNumber);
+                } catch (NumberFormatException e){
+                    throw new MyArrayDataException("В ячейке лежит символ или текст вместо числа");
+                }
+            }
+        }
+
+        System.out.println("Сумма элементов массива = " + sum);
+    }
+
+    public static void checkArray(String [][] array){
+        if (!(array.length == 4 && array[0].length == 4)) throw new MyArraySizeException("Массив должен быть размером 4х4");
+    }
+}
---
+++ src/main/java/com/geekbrains/georgy/lesson4/MyArrayDataException.java
@@ -0,0 +1,9 @@
+package com.geekbrains.georgy.lesson4;
+
+
+public class MyArrayDataException extends NumberFormatException {
+
+    public MyArrayDataException(String s) {
+        super(s);
+    }
+}
